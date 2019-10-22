17c1ee68125031d79c87e9276a7317fbddf11f19
区振轩 <442910407@qq.com>
2019-10-22 20:37:36 +0800

    20有效的括号。用的栈，map数据结构。4 ms，8.6 MB

---
+++ 14/20ValidParentheses/20ValidParentheses.cpp
@@ -0,0 +1,54 @@
+﻿// 20ValidParentheses.cpp: 定义应用程序的入口点。
+//
+
+#include "20ValidParentheses.h"
+#include <stack>
+#include <map>
+
+using namespace std;
+
+bool isValid(string s) {
+	map<char, char> myMap = { { ']', '['} ,{'}','{'},{')','('} };
+
+	stack <char> myStack;
+	//for each (char c in s) 用的这个 力扣编译不过
+	for (char c : s) {
+		map<char, char>::iterator iter;
+		iter = myMap.find(c);
+		if (myMap.find(c) != myMap.end())
+		{//找到了，看栈顶是不是对应的value
+			if (myStack.size() == 0)
+			{
+				return false;
+			}
+			if (myStack.top() == myMap.find(c)->second)
+			{
+				myStack.pop();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{//没找到，入栈
+			myStack.push(c);
+		}
+	} 
+	if (myStack.size() != 0)
+	{
+		return false;
+	}
+	return true;
+
+}
+
+
+int main()
+{
+	cout << "Hello CMake。" << endl;
+
+	cout << isValid("[]{}");
+
+	return 0;
+}
