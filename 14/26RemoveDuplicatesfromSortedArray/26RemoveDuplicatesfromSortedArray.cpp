42bcc7eaedf0f212556ba6cdb3931abcc519390d
区振轩 <442910407@qq.com>
2019-10-23 17:28:05 +0800

    20排序数组中的重复项，执行用时 :24 ms, 在所有 cpp 提交中击败了94.51%的用户 用的上游标思想

---
+++ 14/26RemoveDuplicatesfromSortedArray/26RemoveDuplicatesfromSortedArray.cpp
@@ -0,0 +1,39 @@
+﻿// 26RemoveDuplicatesfromSortedArray.cpp: 定义应用程序的入口点。
+//
+
+#include "26RemoveDuplicatesfromSortedArray.h"
+#include <vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+	if (nums.size()<1)
+	{
+		return 0;
+	}
+	int swagP = 1;
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (nums[i-1] != nums[i])
+		{
+			if (swagP != i)
+			{
+				nums[swagP] = nums[i];
+			}
+			swagP++;
+		} 
+	}
+	return swagP;
+
+
+}
+
+int main()
+{
+	cout << "Hello CMake。" << endl;
+
+	vector<int> nums = { 1,2,2,2 };
+	int num = removeDuplicates(nums);
+	cout << num;
+	return 0;
+}
