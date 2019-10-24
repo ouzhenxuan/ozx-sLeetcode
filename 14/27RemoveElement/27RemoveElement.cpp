2a1d15f008e460df710256c40edc89df0b7635e5
区振轩 <442910407@qq.com>
2019-10-24 15:14:30 +0800

    双指针法，运行效果不错。行用时 :0 ms 内存消耗 : 8.7 MB

---
+++ 14/27RemoveElement/27RemoveElement.cpp
@@ -0,0 +1,30 @@
+﻿// 27RemoveElement.cpp: 定义应用程序的入口点。
+//
+
+#include "27RemoveElement.h"
+#include <vector>
+
+using namespace std;
+
+
+int removeElement(vector<int>& nums, int val) {
+	int flag = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (nums[i] != val)
+		{
+			nums[flag] = nums[i];
+			flag++;
+		}
+	}
+	return flag;
+}
+
+int main()
+{
+	vector<int> nums = { 0,1};
+	
+	cout << "Hello CMake。" << endl;
+	cout << removeElement(nums, 2);
+	return 0;
+}
