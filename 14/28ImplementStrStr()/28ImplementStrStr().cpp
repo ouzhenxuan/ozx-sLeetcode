92be6f0d212723052700efca852543a5d642ff1c
区振轩 <442910407@qq.com>
2019-10-31 20:33:23 +0800

    一开始用的双指针，像沙雕。后面改用循环匹配，耗时4ms。效果不错

---
+++ 14/28ImplementStrStr()/28ImplementStrStr().cpp
@@ -0,0 +1,44 @@
+﻿// 28ImplementStrStr().cpp: 定义应用程序的入口点。
+//
+
+#include "28ImplementStrStr().h"
+
+using namespace std;
+
+int strStr(string haystack, string needle) {
+	if (needle == "")
+	{
+		return 0;
+	}
+	else if (needle.length() > haystack.length())
+	{
+		return -1;
+	}
+	int index = -1;
+	for (int i = 0; i < haystack.length(); i++)
+	{
+		char ch = haystack[i];
+		if (i + needle.length() > haystack.length() )
+		{
+			return -1;
+		}
+		if (ch == needle[0])
+		{
+			string partS = haystack.substr(i, needle.length());
+			if (partS == needle)
+			{
+				return i;
+			}
+		}
+	}  
+	return index;
+}
+
+
+int main()
+{
+	cout << strStr("mississippi", "issip") << endl;
+	
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
