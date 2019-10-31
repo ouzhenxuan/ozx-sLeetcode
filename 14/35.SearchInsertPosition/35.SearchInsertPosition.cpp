839b5a599954f272ed1b7d68d093e41db195a056
区振轩 <442910407@qq.com>
2019-10-31 21:24:24 +0800

    遍历一次完事了。8ms。这不是特别的简单？虽然效果一般

---
+++ 14/35.SearchInsertPosition/35.SearchInsertPosition.cpp
@@ -0,0 +1,33 @@
+﻿// 35.SearchInsertPosition.cpp: 定义应用程序的入口点。
+//
+
+#include "35.SearchInsertPosition.h"
+#include <vector>
+
+using namespace std;
+
+
+int searchInsert(vector<int>& nums, int target) {
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+
+		if (target <= nums[i])
+		{
+			return i;
+		}
+	}
+	return nums.size();
+
+}
+
+
+int main()
+{
+
+	vector<int> nums = {  };
+
+	cout << searchInsert(nums,7) << endl;
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
