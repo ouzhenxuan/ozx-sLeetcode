d46909b13861904b43452d70040ef6f91c63dd59
区振轩 <442910407@qq.com>
2019-11-01 17:34:59 +0800

    报数 递归实现，参考的别人算法，后续继续看看

---
+++ 14/38.CountandSay/38.CountandSay.cpp
@@ -0,0 +1,39 @@
+﻿// 38.CountandSay.cpp: 定义应用程序的入口点。
+//
+
+#include "38.CountandSay.h"
+#include <map>
+#include <string> 
+
+using namespace std;
+
+string countAndSay(int n) {
+	if (n == 1)
+		return "1";
+	string laststr = countAndSay(n - 1);  //通过调用该函数得到上一次的字符串
+	string curstr;
+	int count = 1;
+	for (int i = 0; laststr[i] != '\0'; i++)
+	{
+
+		if (laststr[i] == laststr[i + 1])
+		{
+			count++;
+			continue;
+		}
+		else
+		{
+			curstr += to_string(count) + laststr[i];
+			count = 1;    //对下一个元素重新计数
+		}
+	}
+	return curstr;
+
+}
+
+int main()
+{
+	cout << countAndSay(6) << endl;
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
