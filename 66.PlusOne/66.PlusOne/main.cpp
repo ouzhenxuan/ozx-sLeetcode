371f178828a6c0144d0caa737ad7119886560db8
ozx <442910407@qq.com>
2019-11-07 22:14:52 +0800

    66加一
    优化求解步骤，总体思路不变。

--- 66.PlusOne/66.PlusOne/main.cpp
+++ 66.PlusOne/66.PlusOne/main.cpp
@@ -13,27 +13,27 @@ using namespace std;
 
 vector<int> plusOne(vector<int>& digits) {
     
-    int c  = 0;
     for (int i = (int)digits.size()-1; i>=0; i--) {
-        digits[i] += 1 ;
-        c = digits[i]/10;
-        
-        digits[i] = digits[i]%10;
-        
-        if (c == 0) {
+
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        }else{
+            digits[i]++;
             break;
         }
     }
 
-    if (c != 0) {
-        digits.insert(digits.begin(), 1);
+    if (digits[0] == 0) {
+        digits.push_back(0);
+        digits[0] = 1;
     }
     return digits;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    vector<int> iii = {9};
+    vector<int> iii = {9,9,9};
+//    vector<int> iii = {9,8};
     
     vector<int> jjj =  plusOne(iii);
     for( int var : jjj){
